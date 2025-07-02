8e59e7e71d2dae4e28c747f9db839b3119dda7ff
Samson Mumba <samsonmumba688@gmail.com>
2025-07-02 15:31:31 -0500

    simplified the methods

--- Network_scan.sh
+++ Network_scan.sh
@@ -1,48 +1,48 @@
 #!/usr/bin/bash
 
 
-echo "Enter subnet & output file: (e.g 127.0.0/24 ips.txt) "
-read subnet output_file
+echo "Enter subnet: ( e.g 127.1.2.0/24 )"
+read subnet
 
 
 
 scan_network()
 {
 local subnet_ip=$1
-local output_file=$2
 
 if [[ -z "$subnet_ip" ]]
 then echo "Specify subnet ip"
 
 return 1
-fi
-
-if [[ -z "$output_file" ]]
-then 
-sudo nmap -sn 192.168.183.0/24 | grep 'Nmap scan report' | awk '{print $5}' > ips.txt
-echo " Nmap scan is complete! check output in $output_file"
-return 1
 else
 
-sudo nmap -sn 192.168.183.0/24 | grep 'Nmap scan report' | awk '{print $5}' > "$output_file"
+sudo nmap -sn 192.168.183.0/24 | grep 'Nmap scan report' | awk '{print $5}' > hosts.txt
 
 fi
-
 }
 
 
 
 screenshot()
 {
-  awk '{print "http://" $0}' "$output_file" > url.txt
+  awk '{print "http://" $0}' hosts.txt > url.txt
   eyewitness --web -f url.txt -d Finding --no-prompt
-  mv ips.txt url.txt geckodriver.log Finding/
-  echo " Nmap scan is complete! check output in Findings $output_file file"
-
+  mv hosts.txt url.txt geckodriver.log Finding/
+  echo " Nmap scan is complete! check output in Findings hosts.txt"
+ 
 }
 
+httpRecon()
+{
+   
+  cd Finding
+  grep "Successful" Requests.csv | awk -F',' '{print $3}' > http200.txt
 
+  echo "Recon is done!"
+}
 
 # call
-scan_network "$subnet" "$output_file"
+scan_network "$subnet"
 screenshot
+httpRecon
+
