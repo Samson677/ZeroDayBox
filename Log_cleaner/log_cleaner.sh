a56417de1a83458b8974055ad6cf50bf6bce08a4
Samson Mumba <samsonmumba688@gmail.com>
2025-07-11 12:32:03 -0500

    added a line to clear journal log

--- Log_cleaner/log_cleaner.sh
+++ Log_cleaner/log_cleaner.sh
@@ -3,6 +3,8 @@ log_cleaner()
 {
 echo "Cleaning logs ..."
 sudo find  /var/log -type f \( -name "*.log" -o -name "btmp" -o -name "lastlog" -o -name "wtmp" \) -exec truncate -s 0 {} \;
+sudo journalctl --rotate
+sudo journalctl --vacuum-time=1s
 
 echo "log content erased!"
 
